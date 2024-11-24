a990e7f41199fcf1c1b5f80f3110336b56722193
Samuel Eskilson <samuel.eskilson@gmail.com>
2024-11-24 17:36:00 +0100

    added asteroid prefab

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Start() {
+        body = GetComponent<Rigidbody2D>();
+
+        Vector2 initialDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        float magnitude = Random.Range(20f, 40f);
+        float torque = Random.Range(-10f, 10f);
+
+        body.AddForce(initialDirection * magnitude);
+        body.AddTorque(torque);
+    }
+}
