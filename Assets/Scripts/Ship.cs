796fc731e6433cf5d88c7ad1696a2327bf5e5353
Samuel Eskilson <samuel.eskilson@gmail.com>
2024-11-27 16:13:55 +0100

    implemented thruster particle management

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ship : MonoBehaviour
 {
     [SerializeField]
-    private ParticleSystem
-        RightBackThruster,
-        LeftBackThruster,
-        RightSideThruster,
-        LeftSideThruster;
+    private ThrusterGroup
+        backThrusters,
+        frontThrusters,
+        clockwiseThrusters,
+        counterClockwiseThrusters,
+        rightSideThrusters,
+        leftSideThrusters;
 
     [SerializeField]
     private float
@@ -22,10 +25,13 @@ public class Ship : MonoBehaviour
         stoppingThrust,
         stoppingTorque;
 
+    [SerializeField, Range(0, 1)]
+    private float adjustmentFactor;
+
     private float
         currentThrust,
         currentTorque;
-    private float boostedForwardThrust => boosting ? forwardThrust * boostFactor : forwardThrust;
+    private float boostedForwardThrust => boosting && !stopping ? forwardThrust * boostFactor : forwardThrust;
 
     private bool
         accelerating,
@@ -37,23 +43,40 @@ public class Ship : MonoBehaviour
         boosting,
         stopping;
 
-    [SerializeField]
     private Rigidbody2D body;
 
+    private void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
     private void UpdateThrusterParticles() {
-        if (accelerating) {
-            RightBackThruster.Play();
-            LeftBackThruster.Play();
-        }
-        else {
-            RightBackThruster.Stop();
-            LeftBackThruster.Stop();
+        if (stopping) {
+            backThrusters.Stop();
+            frontThrusters.Stop();
+            clockwiseThrusters.Stop();
+            counterClockwiseThrusters.Stop();
+            rightSideThrusters.Stop();
+            leftSideThrusters.Stop();
+            return;
         }
 
-        if (strafingStarBoard) LeftSideThruster.Play();
-        else LeftSideThruster.Stop();
-        if (strafingPort) RightSideThruster.Play();
-        else RightSideThruster.Stop();
+        if (accelerating) backThrusters.Play();
+        else backThrusters.Stop();
+
+        if (deaccelerating) frontThrusters.Play();
+        else frontThrusters.Stop();
+
+        if (turningClockwise) clockwiseThrusters.Play();
+        else clockwiseThrusters.Stop();
+
+        if (turningCounterClockwise) counterClockwiseThrusters.Play();
+        else counterClockwiseThrusters.Stop();
+
+        if (strafingStarBoard) leftSideThrusters.Play();
+        else leftSideThrusters.Stop();
+
+        if (strafingPort) rightSideThrusters.Play();
+        else rightSideThrusters.Stop();
     }
 
     void FixedUpdate() {
@@ -66,7 +89,6 @@ public class Ship : MonoBehaviour
                 float breakingTorque = stoppingTorque * -Mathf.Sign(body.angularVelocity);
                 body.AddTorque(breakingTorque * Time.fixedDeltaTime);
             }
-            return;
         }
 
         currentThrust += accelerating ? boostedForwardThrust : 0;
@@ -87,14 +109,26 @@ public class Ship : MonoBehaviour
         }
 
         // prevent ship from exceeding maxVelocity
-        if (body.velocity.magnitude > maxVelocity) body.velocity = body.velocity.normalized * maxVelocity;
-
-        // turn ship clockwise or counterclockwise
-        body.AddTorque(currentTorque * Time.fixedDeltaTime);
+        float realMaxVelocity = maxVelocity;
+        if (body.velocity.magnitude < maxVelocity * adjustmentFactor) {
+            // only reduce the maximum velocity if the current velocity is less than reduced max
+            realMaxVelocity *= (stopping ? adjustmentFactor : 1f);
+        }
+        if (body.velocity.magnitude > realMaxVelocity) body.velocity = body.velocity.normalized * realMaxVelocity;
 
         // prevent ship from exceeding maxAngularVelocity
-        if (body.angularVelocity > maxAngularVelocity) body.angularVelocity = maxAngularVelocity;
-        if (body.angularVelocity < -maxAngularVelocity) body.angularVelocity = -maxAngularVelocity;
+        float realMaxAngularVelocity = maxAngularVelocity;
+        if (body.angularVelocity.Between(-realMaxAngularVelocity, realMaxAngularVelocity)) {
+            // only reduce the maximum angular velocity if the current angular velocity is less than reduced max
+            realMaxAngularVelocity *= (stopping ? adjustmentFactor : 1f);
+        }
+
+        if (!stopping || body.angularVelocity.Between(-realMaxAngularVelocity, realMaxAngularVelocity)) {
+            // turn ship clockwise or counterclockwise
+            body.AddTorque(currentTorque * Time.fixedDeltaTime);
+        }
+        if (body.angularVelocity > realMaxAngularVelocity) body.angularVelocity = realMaxAngularVelocity;
+        if (body.angularVelocity < -realMaxAngularVelocity) body.angularVelocity = -realMaxAngularVelocity;
 
         currentThrust = 0;
         currentTorque = 0;
