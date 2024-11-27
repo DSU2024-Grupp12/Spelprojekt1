9e73831945763018e279a53f5251bff4a3feb4cd
Samuel Eskilson <samuel.eskilson@gmail.com>
2024-11-27 16:11:58 +0100

    created utilties directory

---
+++ Assets/Scripts/Utilities/MathExtensions.cs
@@ -0,0 +1,25 @@
+public static class MathExtensions
+{
+    public static bool Between(this float f, float min, float max, BetweenMode mode = BetweenMode.ExclusiveExclusive) {
+        switch (mode) {
+            case BetweenMode.ExclusiveExclusive:
+                return f > min && f < max;
+            case BetweenMode.ExclusiveInclusive:
+                return f > min && f <= max;
+            case BetweenMode.InclusiveExclusive:
+                return f >= min && f < max;
+            case BetweenMode.InclusiveInclusive:
+                return f >= min && f <= max;
+            default: return false;
+        }
+    }
+
+    public enum BetweenMode
+    {
+        ExclusiveExclusive,
+        ExclusiveInclusive,
+        InclusiveExclusive,
+        InclusiveInclusive
+    }
+    
+}
