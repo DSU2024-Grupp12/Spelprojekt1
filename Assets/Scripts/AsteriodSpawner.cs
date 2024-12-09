d500dd7c70811216c9ddf26724bb397fb2c75e1b
Samuel Eskilson <samuel.eskilson@gmail.com>
2024-12-09 17:16:58 +0100

    made asteroid spawning more interesting

--- Assets/Scripts/AsteriodSpawner.cs
+++ Assets/Scripts/AsteriodSpawner.cs
@@ -1,17 +1,31 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = Unity.Mathematics.Random;
 
 public class AsteriodSpawner : MonoBehaviour
 {
+
+    [SerializeField, HideInInspector]
+    private uint numberOfClusters;
+
+    [SerializeField, HideInInspector]
+    private float 
+        minClusterRadius, 
+        maxClusterRadius;
+
+    [SerializeField, HideInInspector]
+    private int 
+        minClusterChildren, 
+        maxClusterChildren;
+
     [SerializeField]
-    private GameObject asteroidPrefab;
+    private Asteroid asteroidPrefab;
 
     [SerializeField, HideInInspector]
     private uint seed, numberOfAsteroids;
 
     [SerializeField, HideInInspector]
-    private float width, height;
+    private float radius;
 
     [SerializeField, HideInInspector]
     private MassDistribution distributionFunction;
@@ -19,54 +33,44 @@ public class AsteriodSpawner : MonoBehaviour
     [SerializeField, HideInInspector, Min(0)]
     private float mean, standardDeviation, minMass, maxMass;
 
+    [HideInInspector]
+    public List<Vector3> clusterPositions;
+
     void Start() {
         CreateAsteroids();
+    }
+
+    public void CreateClusters(ref Random random) {
+        clusterPositions = new();
+        for (int i = 0; i < numberOfClusters; i++) {
+            Vector3 origin = GetRandomPositionInRadius(ref random, radius, Vector3.zero);
+            clusterPositions.Add(origin);
+            float clusterRadius = minClusterRadius + (maxClusterRadius - minClusterRadius) * random.NextFloat();
+            int number = random.NextInt(minClusterChildren, maxClusterChildren);
+            for (int j = 0; j < number; j++) {
+                Vector3 position = GetRandomPolarCoordinate(ref random, 1f, clusterRadius, origin);
+                CreateAsteroid(ref random, position);
+            }
+        }
     }
 
     public void CreateAsteroids() {
         ClearAsteroids();
 
-        Vector3 startingPosition = new Vector3(-width / 2, -height / 2, 0);
-        Random randomPosition;
-        Random randomMass;
+        Random random;
         if (seed != 0) {
-            randomPosition = new Random(seed);
-            randomMass = new Random(seed);
+            random = new Random(seed);
         }
         else {
             uint randomSeed = (uint)UnityEngine.Random.Range(1, int.MaxValue);
-            randomPosition = new Random(randomSeed);
-            randomMass = new Random(randomSeed);
+            random = new Random(randomSeed);
         }
 
+        CreateClusters(ref random);
+
         for (int i = 0; i < numberOfAsteroids; i++) {
-            GameObject asteroid = Instantiate(asteroidPrefab, transform, true);
-
-            // randomize position
-            float randomX = randomPosition.NextFloat() * width;
-            float randomY = randomPosition.NextFloat() * height;
-            asteroid.transform.position = startingPosition + new Vector3(randomX, randomY, 0);
-
-            // randomize rotation
-            asteroid.transform.eulerAngles = new Vector3(0, 0, randomPosition.NextFloat() * 360);
-
-            // randomize mass
-            Rigidbody2D body = asteroid.GetComponent<Rigidbody2D>();
-
-            switch (distributionFunction) {
-                case MassDistribution.Linear:
-                    body.mass = minMass + randomMass.NextFloat() * (maxMass - minMass);
-                    break;
-                case MassDistribution.Normal:
-                    body.mass = NextGaussian(ref randomMass, mean, standardDeviation);
-                    body.mass = Mathf.Clamp(body.mass, minMass, maxMass);
-                    break;
-            }
-
-            // adjust scale according to mass
-            float massAtScale1 = asteroidPrefab.GetComponent<Rigidbody2D>().mass;
-            float cubeSquareScale = Mathf.Sqrt(body.mass / massAtScale1);
-            asteroid.transform.localScale = new Vector3(cubeSquareScale, cubeSquareScale, 1);
+            Vector3 position = GetRandomPositionInRadius(ref random, radius, Vector3.zero);
+            CreateAsteroid(ref random, position);
         }
     }
 
@@ -74,6 +78,58 @@ public class AsteriodSpawner : MonoBehaviour
         for (int i = transform.childCount - 1; i >= 0; i--) {
             DestroyImmediate(transform.GetChild(0).gameObject);
         }
+    }
+
+    private void CreateAsteroid(ref Random random, Vector3 position) {
+        Asteroid asteroid = Instantiate(asteroidPrefab, transform, true);
+        asteroid.transform.position = position;
+
+        // randomize rotation
+        asteroid.transform.eulerAngles = new Vector3(0, 0, random.NextFloat() * 360);
+
+        // randomize mass
+        Rigidbody2D body = asteroid.GetComponent<Rigidbody2D>();
+
+        switch (distributionFunction) {
+            case MassDistribution.Linear:
+                body.mass = minMass + random.NextFloat() * (maxMass - minMass);
+                break;
+            case MassDistribution.Normal:
+                body.mass = NextGaussian(ref random, mean, standardDeviation);
+                body.mass = Mathf.Clamp(body.mass, minMass, maxMass);
+                break;
+        }
+
+        // adjust scale according to mass
+        float massAtScale1 = asteroidPrefab.GetComponent<Rigidbody2D>().mass;
+        float cubeSquareScale = Mathf.Sqrt(body.mass / massAtScale1);
+        asteroid.transform.localScale = new Vector3(cubeSquareScale, cubeSquareScale, 1);
+
+        // randomize angular velocity
+        body.angularVelocity = random.NextFloat() * 10f;
+    }
+
+    private Vector3 GetRandomPositionInRadius(ref Random random, float r, Vector3 origin) {
+        Vector3 startingPosition = new Vector3(-r + origin.x, -r + origin.y, 0);
+
+        Vector3 position;
+        do {
+            float randomX = random.NextFloat() * radius * 2;
+            float randomY = random.NextFloat() * radius * 2;
+            position = startingPosition + new Vector3(randomX, randomY, 0);
+        } while ((position - origin).magnitude > r);
+
+        return position;
+    }
+
+    private Vector3 GetRandomPolarCoordinate(ref Random random,
+                                             float minMagnitude,
+                                             float maxMagnitude,
+                                             Vector2 origin) {
+        float angle = 2 * Mathf.PI * random.NextFloat();
+        float magnitude = minMagnitude + (maxMagnitude - minMagnitude) * random.NextFloat();
+        Vector2 coord = new Vector2(magnitude * Mathf.Cos(angle), magnitude * Mathf.Sin(angle));
+        return origin + coord;
     }
 
     public enum MassDistribution
@@ -87,8 +143,8 @@ public class AsteriodSpawner : MonoBehaviour
         // Random rand = new Random(); //reuse this if you are generating many
         double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
         double u2 = 1.0 - rand.NextDouble();
-        double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
-                               Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+        double randStdNormal = System.Math.Sqrt(-2.0 * System.Math.Log(u1)) *
+                               System.Math.Sin(2.0 * System.Math.PI * u2); //random normal(0,1)
         double randNormal =
             _mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
         return (float)randNormal;
--- Assets/Scripts/Editor/AsteroidSpawnerEditor.cs
+++ Assets/Scripts/Editor/AsteroidSpawnerEditor.cs
@@ -6,10 +6,16 @@ public class AsteroidSpawnerEditor : Editor
 {
     private AsteriodSpawner spawner;
 
+    private bool showClusterParameters;
+    private SerializedProperty numberOfClusters;
+    private SerializedProperty minClusterRadius;
+    private SerializedProperty maxClusterRadius;
+    private SerializedProperty minClusterChildren;
+    private SerializedProperty maxClusterChildren;
+
     private SerializedProperty seed;
     private SerializedProperty numberOfAsteroids;
-    private SerializedProperty width;
-    private SerializedProperty height;
+    private SerializedProperty radius;
     private SerializedProperty dist;
     private SerializedProperty mean;
     private SerializedProperty standardDevitation;
@@ -19,10 +25,15 @@ public class AsteroidSpawnerEditor : Editor
     private void OnEnable() {
         spawner = target as AsteriodSpawner;
 
+        numberOfClusters = serializedObject.FindProperty("numberOfClusters");
+        minClusterRadius = serializedObject.FindProperty("minClusterRadius");
+        maxClusterRadius = serializedObject.FindProperty("maxClusterRadius");
+        minClusterChildren = serializedObject.FindProperty("minClusterChildren");
+        maxClusterChildren = serializedObject.FindProperty("maxClusterChildren");
+
         seed = serializedObject.FindProperty("seed");
         numberOfAsteroids = serializedObject.FindProperty("numberOfAsteroids");
-        width = serializedObject.FindProperty("width");
-        height = serializedObject.FindProperty("height");
+        radius = serializedObject.FindProperty("radius");
         dist = serializedObject.FindProperty("distributionFunction");
         mean = serializedObject.FindProperty("mean");
         standardDevitation = serializedObject.FindProperty("standardDeviation");
@@ -33,9 +44,20 @@ public class AsteroidSpawnerEditor : Editor
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
 
+        showClusterParameters = EditorGUILayout.Foldout(showClusterParameters, "Clusters");
+
+        if (showClusterParameters) {
+            EditorGUILayout.PropertyField(numberOfClusters);
+            EditorGUILayout.PropertyField(minClusterRadius);
+            EditorGUILayout.PropertyField(maxClusterRadius);
+            EditorGUILayout.PropertyField(minClusterChildren);
+            EditorGUILayout.PropertyField(maxClusterChildren);
+        }
+
+        EditorGUILayout.Separator();
+
         EditorGUILayout.PropertyField(numberOfAsteroids);
-        EditorGUILayout.PropertyField(width);
-        EditorGUILayout.PropertyField(height);
+        EditorGUILayout.PropertyField(radius);
 
         EditorGUILayout.PropertyField(seed);
         EditorGUILayout.PropertyField(dist);
@@ -55,5 +77,9 @@ public class AsteroidSpawnerEditor : Editor
 
         if (GUILayout.Button("Create Asteroids")) spawner!.CreateAsteroids();
         if (GUILayout.Button("Clear Asteroids")) spawner!.ClearAsteroids();
+    }
+
+    public void OnInspectorUpdate() {
+        this.Repaint();
     }
 }
